157f26b1390dbe2df2d2470f9d2b8cd00109fdf6
Andreea Sandu <asandu@yammer-inc.com>
2013-07-11 10:29:07 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wim::Application.config.secret_key_base = '6b80aebaf0189f40c1f6a16790d18cca7d8a2bb0b1095024241b34151b904f1f1b79a085fbef02ca7c2cff3acb3180b1b9dcbe0bda695b90b34a2019fa1a0564'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wim::Application.config.session_store :cookie_store, key: '_wim_session'
